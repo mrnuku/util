b6453e9e3febe98e57dd7bbdec3f6714692f98ca
robert.balint <robert.balint@incepteam.com>
2016-03-08 18:31:17 +0100

    LogManager unheroize

--- LogManager.h
+++ LogManager.h
@@ -1,23 +1,23 @@
 //
 //  LogManager.h
-//  McK.HERO.Demo
+//  util
 //
 //  Created by Bálint Róbert on 24/11/15.
 //  Copyright © 2015 Incepteam. All rights reserved.
 //
 
 #import "Manager.h"
+#import <stdio.h>
 
 size_t memoryUsage(void);
 double memoryUsageMegabytes();
 BOOL isDebuggerAttached(void);
 NSString *platform(BOOL stripModel);
+int copyFile(const char *fn1, const char *fn2);
 
 @interface LogManager : Manager
 
-#if HERO
-- (void)uploadLogWithCompletion:(void (^ _Nullable)(void))completion;
-#endif
-- (void)resetUploadedState;
+@property (nonatomic, strong) NSString *redirectedFileName;
+@property (nonatomic) FILE *redirectedFile;
 
 @end
--- LogManager.m
+++ LogManager.m
@@ -1,15 +1,12 @@
 //
 //  LogManager.m
-//  McK.HERO.Demo
+//  util
 //
 //  Created by Bálint Róbert on 24/11/15.
 //  Copyright © 2015 Incepteam. All rights reserved.
 //
 
 #import "LogManager.h"
-#if HERO
-#import "CommunicationManager.h"
-#endif
 #import <sys/sysctl.h>
 #import <mach/mach.h>
 #import "UtilMacros.h"
@@ -151,101 +148,22 @@ int copyFile(const char *fn1, const char *fn2) {
     return all > 0;
 }
 
-@implementation LogManager {
-    NSString *redirectedFileName;
-    FILE *redirectedFile;
-}
+@implementation LogManager
 
 - (instancetype)init {
     self = [super init];
     
     if (self) {
-//#ifdef DEBUG
-        redirectedFileName = [NSString stringWithFormat:@"console-%@.log", [UIDevice currentDevice].identifierForVendor.UUIDString];
-        redirectedFile = redirectConsole(redirectedFileName);
+        //#ifdef DEBUG
+        self.redirectedFileName = [NSString stringWithFormat:@"console-%@.log", [UIDevice currentDevice].identifierForVendor.UUIDString];
+        self.redirectedFile = redirectConsole(self.redirectedFileName);
         TestLog(@"=== LogManager initialized ===");
         TestLog(@"NSDocumentDirectory: %@", NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject);
-//#endif
+        TestLog(@"Current memory usage: %.2f", memoryUsageMegabytes());
+        //#endif
     }
     
     return self;
-}
-
-#if HERO
-- (void)uploadLogWithCompletion:(void (^ _Nullable)(void))completion {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if (!redirectedFile) {
-            if (completion) {
-                completion();
-            }
-            
-            return;
-        }
-        
-        CommunicationManager *commManager = [CommunicationManager manager];
-        
-        NSURL *url = [[NSURL URLWithString:kBaseUrlStr] URLByAppendingPathComponent:kImageAssetWithoutDataEndPointStr];
-        NSURL *imageAssetEndPointUrl = [url URLByAppendingPathComponent:redirectedFileName.stringByDeletingPathExtension];
-        
-        fflush(stderr);
-        fflush(redirectedFile);
-        
-        NSString *shadowDate = [[NSDate date].description stringByReplacingOccurrencesOfString:@" " withString:@"_"];
-        NSString *uploadingFilePath = [NSTemporaryDirectory() stringByAppendingPathComponent:shadowDate];
-        NSString *logFilePath = [NSTemporaryDirectory() stringByAppendingPathComponent:redirectedFileName];
-        
-        if (!copyFile(logFilePath.UTF8String, uploadingFilePath.UTF8String)) {
-            return;
-        }
-        
-        NSURLComponents *fileUrlComp = [NSURLComponents componentsWithString:uploadingFilePath];
-        fileUrlComp.scheme = @"file";
-        NSURL *fileUrl = fileUrlComp.URL;
-        NSString * mimeTypeStr = @"text/plain";
-        
-        TestLog(@"%@ %@: log upload starting: '%@'", NSStringFromClass(self.class), NSStringFromSelector(_cmd), redirectedFileName);
-        
-        [commManager.sessionManager
-         POST:imageAssetEndPointUrl.absoluteString
-         parameters:nil
-         constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-             [formData
-              appendPartWithFileURL:fileUrl
-              name:@"image"
-              fileName:redirectedFileName
-              mimeType:mimeTypeStr
-              error:nil];
-         }
-         success:^(NSURLSessionDataTask *task, id responseObject) {
-             TestLog(@"%@ %@: log upload successfull", NSStringFromClass(self.class), NSStringFromSelector(_cmd));
-             
-             if (completion) {
-                 completion();
-             }
-         }
-         failure:^(NSURLSessionDataTask *task, NSError *error) {
-             TestLog(@"%@ %@: log upload failed", NSStringFromClass(self.class), NSStringFromSelector(_cmd));
-             
-             if (completion) {
-                 completion();
-             }
-         }];
-        
-        // NODE CRASH TEST
-//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.02 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//            FILE *fp2;
-//            if ((fp2 = fopen(uploadingFilePath.UTF8String, "a+"))) {
-//                fprintf(fp2, "NODE_CRASH\n");
-//                fclose(fp2);
-//            }
-//        });
-        
-    });
-}
-#endif
-
-- (void)resetUploadedState {
-    
 }
 
 @end
